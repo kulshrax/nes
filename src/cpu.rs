fac79a0dfca60e9b7e2912d92854b3ffb145bcef
Arun Kulshreshtha <kulshrax@gmail.com>
2019-04-03 22:30:47 -0700

    Finish Instruction enum

--- src/cpu.rs
+++ src/cpu.rs
@@ -370,6 +370,189 @@ enum Instruction {
     CmpAY(AbsoluteY),
     CmpIX(IndexedIndirect),
     CmpIY(IndirectIndexed),
+
+    // CPX - Compare X Register.
+    CpxI(Immediate),
+    CpxZ(ZeroPage),
+    CpxA(Absolute),
+
+    // CPY - Compare Y Register.
+    CpyI(Immediate),
+    CpyZ(ZeroPage),
+    CpyA(Absolute),
+
+    // DEC - Decrement Memory
+    DecZ(ZeroPage),
+    DecZPX(ZeroPageX),
+    DecA(Absolute),
+    DecAX(AbsoluteX),
+
+    // DEX - Decrement X Register.
+    Dex,
+
+    // DEY - Decrement Y Register.
+    Dey,
+
+    // EOR - Exclusive OR.
+    EorI(Immediate),
+    EorZ(ZeroPage),
+    EorZX(ZeroPageX),
+    EorA(Absolute),
+    EorAX(AbsoluteX),
+    EorAY(AbsoluteY),
+    EorIX(IndexedIndirect),
+    EorIY(IndirectIndexed),
+
+    // INC - Increment Memory,
+    IncZ(ZeroPage),
+    IncZP(ZeroPageX),
+    IncA(Absolute),
+    IncAX(AbsoluteX),
+
+    // INX - Increment X Register.
+    Inx,
+
+    // INY - Increment Y Register.
+    Iny,
+
+    // JMP - Jump.
+    JmpA(Absolute),
+    JmpI(Indirect),
+
+    // JSR - Jump to Subroutine.
+    Jsr(Absolute),
+
+    // LDA - Load Accumulator.
+    LdaI(Immediate),
+    LdaZ(ZeroPage),
+    LdaZX(ZeroPageX),
+    LdaA(Absolute),
+    LdaAX(AbsoluteX),
+    LdaAY(AbsoluteY),
+    LdaIX(IndexedIndirect),
+    LdaIY(IndirectIndexed),
+
+    // LDX - Load X Register.
+    LdxI(Immediate),
+    LdxZ(ZeroPage),
+    LdxZY(ZeroPageY),
+    LdxA(Absolute),
+    LdxAY(AbsoluteY),
+
+    // LDY - Load Y Register.
+    LdyI(Immediate),
+    LdyZ(ZeroPage),
+    LdyZX(ZeroPageX),
+    LdyA(Absolute),
+    LdyAX(AbsoluteX),
+
+    // LSR - Logical Right Shift.
+    LsrAcc(Accumulator),
+    LsrZ(ZeroPage),
+    LsrZX(ZeroPageX),
+    LsrA(Absolute),
+    LsrAX(AbsoluteX),
+
+    // NOP - No Operation.
+    Nop,
+
+    // ORA - Logical Inclusive OR.
+    OraI(Immediate),
+    OraZ(ZeroPage),
+    OraZX(ZeroPageX),
+    OraA(Absolute),
+    OraAX(AbsoluteX),
+    OraAY(AbsoluteY),
+    OraIX(IndexedIndirect),
+    OraIY(IndirectIndexed),
+
+    // PHA - Push Accumulator.
+    Pha,
+
+    // PHP - Push Processor Status.
+    Php,
+
+    // PLA - Pull Accumulator.
+    Pla,
+
+    // PLP - Pull Processor Status.
+    Plp,
+
+    // ROL - Rotate Left.
+    RolAcc(Accumulator),
+    RolZ(ZeroPage),
+    RolZX(ZeroPageX),
+    RolA(Absolute),
+    RolAX(AbsoluteX),
+
+    // ROR - Rotate Right.
+    RorAcc(Accumulator),
+    RorZ(ZeroPage),
+    RorZX(ZeroPageX),
+    RorA(Absolute),
+    RorAX(AbsoluteX),
+
+    // RTI - Return from Interrupt.
+    Rti,
+
+    // RTS - Return from Subroutine.
+    Rts,
+
+    // SBC - Subtract with Carry.
+    SbcI(Immediate),
+    SbcZ(ZeroPage),
+    SbcZX(ZeroPageX),
+    SbcA(Absolute),
+    SbcAX(AbsoluteX),
+    SbcAY(AbsoluteY),
+    SbcIX(IndexedIndirect),
+    SbcIY(IndirectIndexed),
+
+    // SEC - Set Carry Flag,
+    Sec,
+
+    // SED - Set Decimal Flag,
+    Sed,
+
+    // SEI - Set Interrupt Disable.
+    Sei,
+
+    // STA - Store Accumulator.
+    StaZ(ZeroPage),
+    StaZX(ZeroPageX),
+    StaA(Absolute),
+    StaAX(AbsoluteX),
+    StaAY(AbsoluteY),
+    StaIX(IndexedIndirect),
+    StaIY(IndirectIndexed),
+
+    // STX - Store X Register.
+    StxZ(ZeroPage),
+    StxZY(ZeroPageY),
+    StxA(Absolute),
+
+    // STY - Store X Register.
+    StyZ(ZeroPage),
+    StyZX(ZeroPageX),
+    StyA(Absolute),
+
+    // TAX - Transfer Accumulator to X.
+    Tax,
+
+    // TAY - Transfer Accumulator to Y.
+    Tay,
+
+    // TSX - Transfer Stack Pointer to X.
+    Tsx,
+
+    // TXA - Transfer X to Accumulator.
+    Txa,
+
+    // TXS - Transfer X to Stack Pointer.
+    Txs,
+
+    // TYA - Transfer Y to Accumulator.
+    Tya,
 }
 
 impl Instruction {

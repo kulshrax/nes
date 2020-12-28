2616d4b0c643758e243dcae38d82cd82d933fc6b
Arun Kulshreshtha <kulshrax@gmail.com>
2020-12-28 15:11:19 -0500

    Update to nom 6

--- src/rom.rs
+++ src/rom.rs
@@ -1,11 +1,12 @@
 /// Parser for the iNES ROM format.
-use std::{
-    fs::File,
-    io::{self, prelude::*},
-    path::Path,
-};
+use std::{fs::File, io::prelude::*, path::Path};
 
-use nom::*;
+use anyhow::Result;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u8,
+    IResult,
+};
 
 const PRG_BANK_SIZE: usize = 16384; // 16 KiB
 const CHR_BANK_SIZE: usize = 8192; // 8 KiB
@@ -17,40 +18,51 @@ pub struct Rom {
 }
 
 impl Rom {
-    pub fn load(path: impl AsRef<Path>) -> io::Result<Self> {
+    pub fn load(path: impl AsRef<Path>) -> Result<Self> {
         let mut buf = Vec::new();
         let mut f = File::open(path.as_ref())?;
         f.read_to_end(&mut buf)?;
-        Self::parse(&buf)
-    }
 
-    pub fn parse(bytes: impl AsRef<[u8]>) -> io::Result<Self> {
-        let (_, rom) = rom(bytes.as_ref()).expect("Failed to parse ROM");
-        Ok(rom)
+        // XXX: Nom's error types are a bit complicated and contain references.
+        // Converting to an owned representation (even just converting to an
+        // owned string) is non-trivial. Rather than dealing with this, just
+        // panic if we can't parse the ROM (since we'd want to exit the program
+        // anyway if we can't load the input).
+        let (_, (prg, chr)) = parse_rom(&buf).expect("Failed to parse ROM file");
+
+        Ok(Rom {
+            prg: prg.to_vec(),
+            chr: chr.to_vec(),
+        })
     }
 }
 
-named!(rom<&[u8], Rom>,
-    do_parse!(
-        tag!(b"NES\x1A") >>
-        prg_len: le_u8 >>
-        chr_len: le_u8 >>
-        take!(10) >>
-        prg_data: take!(prg_len as usize * PRG_BANK_SIZE) >>
-        chr_data: take!(chr_len as usize * CHR_BANK_SIZE) >>
-        (Rom {
-            prg: prg_data.to_vec(),
-            chr: chr_data.to_vec(),
-        })
-    )
-);
+/// Identify and return slices containing the ROM file's PRG and CHR sections.
+fn parse_rom(bytes: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+    // Initial 4 byte magic sequence.
+    let (bytes, _) = tag(b"NES\x1A")(bytes.as_ref())?;
+
+    // Length of PRG and CHR sections, specified as a multiple of the
+    // appropriate bank size.
+    let (bytes, prg_len) = le_u8(bytes)?;
+    let (bytes, chr_len) = le_u8(bytes)?;
+
+    // Garbag
+    let (bytes, _) = take(10usize)(bytes)?;
+
+    // Actual PRG and CHR data from the input.
+    let (bytes, prg_data) = take(prg_len as usize * PRG_BANK_SIZE)(bytes)?;
+    let (bytes, chr_data) = take(chr_len as usize * CHR_BANK_SIZE)(bytes)?;
+
+    Ok((bytes, (prg_data, chr_data)))
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn load() -> io::Result<()> {
+    fn load() -> Result<()> {
         let _ = Rom::load("test/nestest.nes")?;
         Ok(())
     }

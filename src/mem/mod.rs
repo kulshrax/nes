2790e7ad6550e8b449a4a8fdc221ae43e47a40ef
Arun Kulshreshtha <kulshrax@gmail.com>
2021-01-26 21:28:45 -0800

    Add initial PPU module

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -6,6 +6,7 @@ mod bus;
 mod mapper;
 
 use crate::rom::Rom;
+use crate::ppu::Ppu;
 
 const RAM_SIZE: usize = 2048;
 
@@ -15,15 +16,13 @@ const PPU_REG_SIZE: usize = 8;
 const IO_REG_START: Address = Address(0x4000);
 const IO_REG_SIZE: usize = 24;
 
-const TEST_MODE_START: Address = Address(0x4018);
-
 const CART_SPACE_START: Address = Address(0x4020);
-const CART_SPACE_SIZE: usize = 1;
+const CART_SPACE_SIZE: usize = 49152; // Remainder of address space.
 
 /// Struct representing the NES's CPU memory bus.
 ///
 /// The NES's CPU address space is laid out as folows:
-///   0x0000 - 0x7FFF: RAM (2kB)
+///   0x0000 - 0x07FF: RAM (2kB)
 ///   0x0800 - 0x1FFF: Mirrors of RAM (repeated 3 times)
 ///   0x2000 - 0x2007: Memory mapped PPU registers (8 total)
 ///   0x2008 - 0x3FFF: Mirrors of PPU registers (every 8 bytes)
@@ -32,37 +31,71 @@ const CART_SPACE_SIZE: usize = 1;
 ///   0x4020 - 0xFFFF: Cartridge address space (PRG ROM, PRG RAM, mappers)
 ///
 pub struct Memory {
-    ram: [u8; RAM_SIZE],
+    ram: Ram,
+    // ppu: Ppu,
+    // rom: Rom,
 }
-
-// impl From<Address> for Index {
-//     fn from(addr: Address) -> Self {
-//         if addr < PPU_REG_START {
-//             Ram(addr % RAM_SIZE)
-//         } else if addr < IO_REG_START {
-//             Ppu(addr % PPU_REG_SIZE)
-//         } else if addr < TEST_MODE_START {
-//             Io(addr % IO_REG_SIZE)
-//         } else if addr < CART_SPACE_START {
-//             panic!("Address {} refers to unmapped memory", addr)
-//         } else {
-//             Cartridge(addr - CART_SPACE_START)
-//         }
-//     }
-// }
 
 impl Memory {
     pub fn new() -> Self {
-        Self { ram: [0; RAM_SIZE] }
+        Self { ram: Ram::new() }
     }
 }
 
 impl Bus for Memory {
     fn load(&self, addr: Address) -> u8 {
-        unimplemented!()
+        if addr < PPU_REG_START {
+            // Read from RAM.
+            let addr = mask(addr, RAM_SIZE);
+            self.ram.load(addr)
+        } else if addr < IO_REG_START {
+            // Read from PPU register.
+            let addr = mask(addr, PPU_REG_SIZE);
+            //self.ppu.load(addr)
+            0
+        } else if addr < CART_SPACE_START {
+            // Read from IO register.
+            let _addr = mask(addr, IO_REG_SIZE);
+            0
+        } else {
+            // Read from cartridge.
+            0
+        }
+        
     }
 
     fn store(&mut self, addr: Address, value: u8) {
         unimplemented!()
     }
+}
+
+struct Ram([u8; RAM_SIZE]);
+
+impl Ram {
+    fn new() -> Self {
+        Ram([0; RAM_SIZE])
+    }
+}
+
+impl Bus for Ram {
+    fn load(&self, addr: Address) -> u8 {
+        self.0[addr.as_usize()]
+    }
+
+    fn store(&mut self, addr: Address, value: u8) {
+        self.0[addr.as_usize()] = value;
+    }
+}
+
+/// Mask off the high order bits for the given address for a memory region of a
+/// given size. The region size must be a power of 2.
+///
+/// On the NES, addresses for each memory-mapped region are incomletely decoded,
+/// meaning that the hardware only reads as many low order address lines (bits)
+/// are needed for the given region size. This results in aliasing, which is why
+/// several of the NES's memory regions are mirrored within the address space.
+fn mask(addr: Address, size: usize) -> Address {
+    // Memory region size must be a power of 2.
+    assert_eq!((size & (size - 1)), 0);
+    addr & (size as u16 - 1)
 }
--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,25 +1,82 @@
-use crate::mem::{Address, Bus};
+pub enum PpuRegister {
+    Ctrl,
+    Mask,
+    Status,
+    Scroll,
+    Addr,
+    Data,
+    OamAddr,
+    OamData,
+    OamDma,
+}
+
+#[derive(Default)]
+struct Registers {
+    ctrl: u8,
+    mask: u8,
+    status: u8,
+    scroll: u8,
+    addr: u8,
+    data: u8,
+    oam_addr: u8,
+    oam_data: u8,
+    oam_dma: u8,
+}
+
+impl Registers {
+    fn get(&self, reg: PpuRegister) -> u8 {
+        use PpuRegister::*;
+        match reg {
+            Ctrl => self.ctrl,
+            Mask => self.mask,
+            Status => self.status, 
+            Scroll => self.scroll,
+            Addr => self.addr,
+            Data => self.data,
+            OamAddr => self.oam_addr,
+            OamData => self.oam_data,
+            OamDma => self.oam_dma,
+        }
+    } 
+
+    fn get_mut(&mut self, reg: PpuRegister) -> &mut u8 {
+        use PpuRegister::*;
+        match reg {
+            Ctrl => &mut self.ctrl,
+            Mask => &mut self.mask,
+            Status => &mut self.status, 
+            Scroll => &mut self.scroll,
+            Addr => &mut self.addr,
+            Data => &mut self.data,
+            OamAddr => &mut self.oam_addr,
+            OamData => &mut self.oam_data,
+            OamDma => &mut self.oam_dma,
+        }
+    } 
+}
 
 pub struct Ppu {
-    ram: [u8; 2048],
+    registers: Registers,
+    vram: [u8; 2048],
     oam: [u8; 256],
+    palette: [u8; 32],
 }
 
 impl Ppu {
     pub fn new() -> Self {
         Self {
-            ram: [0u8; 2048],
-            oam: [0u8; 256],
+            registers: Registers::default(),
+            vram: [0; 2048],
+            oam: [0; 256],
+            palette: [0; 32],
         }
     }
-}
 
-impl Bus for Ppu {
-    fn load(&self, addr: Address) -> u8 {
-        unimplemented!()
-    }
+    pub fn read(&self, reg: PpuRegister) -> u8 {
+        self.registers.get(reg)
+    } 
 
-    fn store(&mut self, addr: Address, value: u8) {
-        unimplemented!()
+    pub fn write(&mut self, reg: PpuRegister, value: u8) {
+        *self.registers.get_mut(reg) = value;
     }
 }

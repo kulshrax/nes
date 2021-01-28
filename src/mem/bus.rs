d410d6950c230ca5e4cb85e59586cf144c6ad4b4
Arun Kulshreshtha <kulshrax@gmail.com>
2021-01-27 21:34:03 -0800

    Update comments in bus module

--- src/mem/bus.rs
+++ src/mem/bus.rs
@@ -1,14 +1,16 @@
 use super::address::Address;
 
-/// Trait representing an address bus, which can be used by the CPU to read and
-/// write values. Note that the actual destination of these writes can be mapped
-/// by hardware mappers on the cartridge, which is why this needs to be a trait.
+/// Trait representing the CPU's address bus. The actual destination of loads
+/// and stores are mapped by hardware to several possible locations, including
+/// the NES's RAM, the PPU, various IO registers, or the cartridge, which in
+/// turn can arbitrarily map the memory access to its contents.
 pub trait Bus {
     fn load(&self, addr: Address) -> u8;
 
     fn store(&mut self, addr: Address, value: u8);
 }
 
+/// It can be useful to treat the 16-bit address space as an array for testing.
 impl Bus for [u8; 0x10000] {
     fn load(&self, addr: Address) -> u8 {
         self[addr.as_usize()]

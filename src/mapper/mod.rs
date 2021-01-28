e53a3f7a88c5ce41d9085bbaa2bdf10479afe24d
Arun Kulshreshtha <kulshrax@gmail.com>
2021-01-27 22:28:33 -0800

    Add comment for Mapper trait

--- src/mapper/mod.rs
+++ src/mapper/mod.rs
@@ -2,6 +2,14 @@ use crate::mem::Address;
 
 mod mapper0;
 
+/// Mappers are hardware on the game cartridge that map memory accesses from the
+/// CPU and PPU to the cartridge's ROM and RAM chips. By using techniques such
+/// as bank switching, mappers can allow the total size of the game's data to be
+/// larger than the NES's available address space.
+///
+/// Since mappers will receive memory accesses from both the CPU and PPU (each
+/// of which has its own address space), this trait has separate CPU and PPU
+/// methods to make it easy to distinguish the source of the memory access.
 pub trait Mapper {
     fn cpu_load(addr: Address) -> u8;
 

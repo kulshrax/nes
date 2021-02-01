b98b6aef736c7f6d9691d904b11540b3724a9e9d
Arun Kulshreshtha <kulshrax@gmail.com>
2021-01-31 16:48:55 -0800

    Add PpuBus trait

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,5 +1,7 @@
 use crate::mem::{Address, Bus};
 
+pub const VRAM_SIZE: usize = 2048;
+
 // Since there are only 8 PPU registers, only the last 3 address bits are used
 // to determine which register to select.
 const PPU_REG_ADDR_BITS: u8 = 3;
@@ -16,9 +18,20 @@ pub enum PpuRegister {
     Data = 7,
 }
 
+/// Trait representing the PPU's address bus, which is used to access the PPU's
+/// address space (separate from the CPU addres space). PPU memory accesses can
+/// be arbitrarily remapped by the cartridge, which is why a reference to the
+/// PPU's VRAM is passed into these methods (so that the mapper can choose to
+/// map a read or write to VRAM).
+pub trait PpuBus {
+    fn load(&self, vram: &[u8; VRAM_SIZE], addr: Address) -> u8;
+
+    fn store(&mut self, vram: &mut [u8; VRAM_SIZE], addr: Address, value: u8);
+}
+
 pub struct Ppu {
     registers: [u8; 8],
-    vram: [u8; 2048],
+    vram: [u8; VRAM_SIZE],
     oam: [u8; 256],
     palette: [u8; 32],
 }
